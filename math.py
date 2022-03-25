ea58293c992f71a9046cb48f6dc0d181b4343140
Sachin Lulla <saigovindtextile@gmail.com>
2022-03-25 23:32:01 +0530

    fn names added

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+# add function implemented
 def add(x,y):
     pass
 
+# subtract fn implemented
 def sub(x,y):
     pass
 
+# multiply fn implemented
 def mul(x,y):
     pass
 
+#divide fn implemented
 def div(x,y):
     pass
